c4259c5de88ac2bd1bc38b182c354024a6acad69
R1m7PY <bibik.hg.15@gmail.com>
2022-12-15 16:49:27 +0500

    MVP v1.0

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from request import start_parse
+import argparse
 
 
-def main():
-    url = input("url address: ")
-    print(start_parse(url))
+# def main():
+#     print(start_parse())
 
 
 if __name__ == "__main__":
-    main()
+    parsargs = argparse.ArgumentParser(description="Practice parser")
+
+    parsargs.add_argument("-u", "--url", dest="url", help="url сайта", type=str, required=True)
+    parsargs.add_argument("-p", "--proxy", dest="proxyfile", help="ссылка на файл с прокси")
+
+    args = parsargs.parse_args()
+    print(args)
+    # main()
+    print(start_parse(args))
--- request/__init__.py
+++ request/__init__.py
@@ -1,95 +1,95 @@
 from random import choice
 from bs4 import BeautifulSoup
 import requests
-import settings
+import argparse
 
 
-DESCTOP_AGENTS = ['Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
-                  'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
-                  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
-                  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14',
-                  'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
-                  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
-                  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
-                  'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
-                  'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
-                  'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0']
-
-proxies = {
-    'http': choice(settings.PROXIES),
-    'https': choice(settings.PROXIES)
-}
+DESCKTOP_AGENTS = ['Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+                   'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+                   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+                   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14',
+                   'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
+                   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
+                   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
+                   'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
+                   'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+                   'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0']
 
 
 def random_headers():
     """
     Возвращает рандомные хедеры для запроса
     """
-    return {'User-Agent': choice(DESCTOP_AGENTS), 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'}
+    return {'User-Agent': choice(DESCKTOP_AGENTS), 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'}
 
 
-def search_href(url: str, urls: list) -> list:
+def search_href(response, urls: list, url: str) -> list:
     """
     Ищет все ссылки на странице и добавляет их в список для дальнейшего парсинга
     """
-    response = requests.get(url, headers=random_headers(),
-                            proxies=proxies, timeout=1.5)
-    if response.status_code != 200:  # проверка статус-кода запроса
-        print(response.status_code)
-        return urls
-
     bs = BeautifulSoup(response.text, "lxml")
     temp = bs.find_all("a")  # поиск всех тегов <a> на странице
     for element in temp:  # итерируемся по всем найденым тегам <a>
         href = element.get("href", "")  # вытаскиваем из тега ссылку
+        if href[0] == "/":
+            href = url + href[1::]  # ссылка на страницу
+        else:
+            if url in href:
+                href = href
         if "#" not in href and href not in urls and "/" in href:
             urls.append(href)
 
     return urls
 
 
-def search_form(url: str) -> int | None:
+def search_form(response) -> int | None:
     """
     Ищет количество форм на странице
     """
-    print(url)
-    response = requests.get(url, headers=random_headers(),
-                            proxies=proxies, timeout=1.5)
-    if response.status_code != 200:  # проверка статус-кода запроса
-        print("None: ", response.status_code)
-        return None
-
     bs = BeautifulSoup(response.text, "lxml")
     temp = bs.find_all("form")
     print(len(temp))
     return len(temp)
 
 
-def start_parse(url: str) -> dict:
+def start_parse(args: argparse.Namespace) -> dict:
     """
     Основная функция запуска парсера
     """
-    urls = list()  # список всех ссылок сайта
+    url = args.url
+    proxy = args.proxyfile
+    if proxy == None:
+        proxylist = None
+    else:
+        with open(proxy) as f:
+            proxylist = f.readlines()
+
+    urls = [url]  # список всех ссылок сайта
     i = int()  # для итерации по списку ссылок
     result = dict()
 
-    search_href(url, urls)
-    # print(urls)
     while i < len(urls):
-        # print(urls[i])
-        if urls[i][0] == "/":
-            url_now = url + urls[i][1:]  # ссылка на страницу
-        else:
-            if url in urls[i]:
-                url_now = urls[i]
-            else:
-                i += 1
-                continue
+        url_now = urls[i]
 
-        k_forms = search_form(url_now)  # количество форм на странице
+        if proxylist != None:
+            proxies = {
+                'http': choice(proxylist),
+                'https': choice(proxylist)
+            }
+        else:
+            proxies = None
+        print(url_now)
+        response = requests.get(url_now, headers=random_headers(),
+                                proxies=proxies, timeout=1.5)
+        if response.status_code != 200:  # проверка статус-кода запроса
+            print(response.status_code)
+            continue
+
+        # количество форм на странице
+        k_forms = search_form(response)
         result[url_now] = k_forms
 
-        search_href(url_now, urls)
+        search_href(response, urls, url)
         i += 1
 
     return result
